c8e55659b588e7f41864e01db66494d759cee061
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-05-26 03:52:44 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-console.log("[JOB] RUNNED: ", new Date().toLocaleString('en-US', {
+const ENDPOINT_URL = "https://workflows-test-13c0a-default-rtdb.firebaseio.com/logs.json";
+const { default: axios } = require("axios");
+
+console.log("[JOB] STARTED: ", new Date().toLocaleString('en-US', {
   timeZone: 'Asia/Yangon',
-});
+}));
+
+axios.post(ENDPOINT_URL, {
+  ...env,
+  createdAt: Date.now(),
+}).then(({ data }) => {
+  console.log('[JOB] STORED: ', data);
+}).catch((e) => {
+  console.error(e, e.response?.data || e.message, e.headers);
+  console.log("[JOB] FAILED: ", e.message);
+}).finally(() => {
+  console.log("[JOB] ENDED: ", new Date().toLocaleString('en-US', {
+    timeZone: 'Asia/Yangon',
+  }));
+});;
