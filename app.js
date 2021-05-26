3d5f7faff2b72adfde69c67d9f3f84841e0346ed
Etherio <50423290+etherio97@users.noreply.github.com>
2021-05-26 21:44:03 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,17 @@
-const ENDPOINT_URL = "https://workflows-test-13c0a-default-rtdb.firebaseio.com/logs.json";
 const { default: axios } = require("axios");
+const Job = require('./Job');
+const ENDPOINT_URL = process.env.ENDPOINT_URL ||
+  "https://workflows-test-13c0a-default-rtdb.firebaseio.com/logs.json";
 
-console.log("[JOB] STARTED: ", new Date().toLocaleString('en-US', {
-  timeZone: 'Asia/Yangon',
-}));
+!function start() {
+  const job = new Job(process.env);
 
-const {
-  GITHUB_ACTION,
-  GITHUB_EVENT_NAME,
-  GITHUB_JOB,
-  GITHUB_RUN_ID: 
-  GITHUB_RUN_NUMBER,
-  GITHUB_SHA,
-  GITHUB_WORKFLOW,
-  RUNNER_TRACKING_ID
-} = process.env;
-
-axios.post(ENDPOINT_URL, {
-  GITHUB_ACTION,
-  GITHUB_EVENT_NAME,
-  GITHUB_JOB,
-  GITHUB_RUN_ID,
-  GITHUB_RUN_NUMBER,
-  GITHUB_SHA,
-  GITHUB_WORKFLOW,
-  RUNNER_TRACKING_ID,
-  createdAt: Date.now(),
-}).then(({ data }) => {
-  console.log('[JOB] STORED: ', data);
-}).catch((e) => {
-  console.error(e, e.response?.data || e.message, e.headers);
-  console.log("[JOB] FAILED: ", e.message);
-}).finally(() => {
-  console.log("[JOB] ENDED: ", new Date().toLocaleString('en-US', {
-    timeZone: 'Asia/Yangon',
-  }));
-});;
+  // store to database
+  axios.post(ENDPOINT_URL, job)
+    .then(({ data }) => {
+      //
+    }).catch((e) => {
+      console.error(e, '-'.repeat(60));
+      console.log(">> Database connection failed!", e.message);
+    });
+}();
