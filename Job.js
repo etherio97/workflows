3ad92ea32c9c1ad15474b790850baf29d3a48ee4
Etherio <50423290+etherio97@users.noreply.github.com>
2021-05-26 21:31:37 +0630

    Create Job.js

---
+++ Job.js
@@ -0,0 +1,23 @@
+class Job {
+  constructor({
+    key,
+    GITHUB_ACTION,
+    GITHUB_EVENT_NAME,
+    GITHUB_JOB,
+    GITHUB_RUN_ID,
+    GITHUB_SHA,
+    GITHUB_WORKFLOW,
+    createdAt
+  }) {
+    this.key = key;
+    this.GITHUB_ACTION = GITHUB_ACTION;
+    this.GITHUB_EVENT_NAME = GITHUB_EVENT_NAME;
+    this.GITHUB_JOB = GITHUB_JOB;
+    this.GITHUB_RUN_ID = GITHUB_RUN_ID;
+    this.GITHUB_SHA = GITHUB_SHA;
+    this.GITHUB_WORKFLOW = GITHUB_WORKFLOW;
+    this.createdAt = new Date(createdAt || Date.now);
+  }
+}
+
+module.exports = Job;
