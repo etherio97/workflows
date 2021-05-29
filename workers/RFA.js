ff092660fd36b2a7ceadef02f9e8047bdb67cb41
Etherio <50423290+etherio97@users.noreply.github.com>
2021-05-30 05:14:55 +0630

    Update RFA.js

--- workers/RFA.js
+++ workers/RFA.js
@@ -10,7 +10,7 @@ const comDetails = {
   homepage: 'https://www.rfa.org/burmese',
   images: {
     logo: 'https://www.nweoo.com/images/logo/rfa.png',
-    cover: 'https://api.nweoo.com/rfa.jpg',
+    thumbnail: 'https://api.nweoo.com/rfa.jpg',
   },
   feeds: [
     {
@@ -35,6 +35,42 @@ const comDetails = {
 };
 
 
+/** @interface */
+class Article {
+  /**
+   * @params {object}
+   */
+  constructor(item) {
+    this.id = item.id || undefined;
+    this.content = item.content || undefined;
+    this.image = item.image || undefined;
+    this.link = item.link || undefined;
+    this.timestamp = item.timestamp || undefined;
+    this.title = item.title || undefined
+    this.source = item && item.source || undefined;
+    this.caption = item.caption || null;
+    this.message_id = item.message_id || null;
+    this.photo_id = item.photo_id || null;
+    this.post_id = item.post_id || null;
+    this.video_id = item.video_id || null;
+  }
+}
+
+
+/** @interface */
+class ArticleContent {
+  /**
+   * @params {object}
+   */
+  constructor(item) {
+    this.content = item.content;
+    this.youtube_id = item.youtube_id;
+  }
+  
+  get hasVideo() {
+    return Boolean(this.youtube_id);
+  }
+}
 
 /**
  * Get RSS Feed for asyncornously in `XML` format
@@ -53,6 +89,15 @@ function getRSSFeed(url) {
       ignoreAttributes: false,
     };
   return axios.get(url, { responseType: 'text' }).then(res => parse(res.data, parseOptions));
+}
+
+/**
+ * @private
+ * @params {string} content
+ * @returns {string}
+ */
+function removeWhitespace(content) {
+  return content.split("\n").map(p => p.trim()).filter(p => !!p).join("\n\n'));
 }
 
 /**
@@ -84,26 +129,68 @@ function fetchArticles() {
 
 /**
  * @private
+ * @params {!string}
+ * @returns {!ArticleContent}
+ */
+function createContent(content) {
+  let youtube_id;
+  let { window } = new JSDOM(content);
+  let { document } = window;
+  let videoFrame = document.querySelector("iframe");
+  
+  if (videoFrame instanceof HTMLElement) {
+    youtube_id = videoFrame.src.split("/").pop();
+    videoFrame.remove();
+  }
+  
+  content = removeWhitespace(content.textContent);
+  
+  return new ArticleContent({
+    content,
+    youtube_id,
+  });
+}
+
+/**
+ * @private
+ * @params {string} date
+ * @returns {number}
+ */
+function publishedDate(datetime) {
+  return new Date(datetime || Date.now).getTime();
+}
+
+/**
+ * @private
  * @params {object} item
- * @returns {object}
+ * @returns {!Article}
  */
 function wrapArticle(item) {
-  const article = {
-    id: undefined,
-    title: undefined,
-    content: undefined,
-    image: undefined,
-    link: undefined,
-    caption: null,
-    source: comDetails.name,
-    timestamp: Date.now(),
-    post_id: null,
-    photo_id: null,
-    video_id: null,
-    message_id: null,
+  let id = (Date.now() - 1600000000000).toString();
+  let { content, isVideo, youtube_id } = createContent(item["content:encoded"].text);
+  let media = item["media:content"] || {};
+  let image = media.attr?.url || comDetails.images.thumbnail;
+  let caption = media['media:description'] && media['media:description'].text;
+
+  return {
+    id,
+    isVideo,
+    youtube_id,
+    data: new Article({
+      id,
+      title: item.title.trim(),
+      content: undefined,
+      image: ,
+      link: item["link"],
+      source: comDetails.name,
+      timestamp: publishedDate(item.pubDate),
+      caption,
+      video_id: null,
+      message_id: null,
+      photo_id: null,
+      post_id: null,
+    }),
   };
-  
-  return article;
 }
 
 /**
@@ -113,8 +200,8 @@ async function fetchAll() {
   const articles = await fetchNews();
   const multimedia = await fetchVideos();
   
-  for (let data of (await fetchAll())) {
-    
+  for (let data of await fetchAll()) {
+    console.log(data)
   }
   
 }
